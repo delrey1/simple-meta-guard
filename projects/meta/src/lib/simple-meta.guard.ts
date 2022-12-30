206fa496722e4f7e4fde7bd6d13f65235395ea20
jorge <j@j>
2022-12-30 10:18:36 +0000

    Adding CanActivateChild

--- projects/meta/src/lib/simple-meta.guard.ts
+++ projects/meta/src/lib/simple-meta.guard.ts
@@ -1,16 +1,17 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
 import {Meta, MetaDefinition, Title} from "@angular/platform-browser";
 
 export interface SeoConfig {
   title: string;
   body: MetaDefinition[];
 }
+
 @Injectable({
   providedIn: 'root'
 })
-export class SimpleMetaGuard implements CanActivate {
+export class SimpleMetaGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private titleService: Title,
@@ -22,6 +23,15 @@ export class SimpleMetaGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     const metaData: SeoConfig = route.data['meta'];
+
+    this.metaService.addTags(metaData.body);
+
+    this.titleService.setTitle(metaData.title);
+    return true;
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const metaData: SeoConfig = childRoute.data['meta'];
 
     this.metaService.addTags(metaData.body);
 
